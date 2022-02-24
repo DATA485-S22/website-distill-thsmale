4d876f297193083feed4646dcf218e079be23f76
Robin Donatello <rdonatello@csuchico.edu>
2022-02-23 21:11:36 -0800

    Revert "Feedback"

--- render_site.R
+++
@@ -1,3 +0,0 @@
-rmarkdown::render("_posts/2022-02-01-find-the-giraffe/find-the-giraffe.Rmd")
-rmarkdown::render("_posts/2022-02-01-how-to-be-an-influencer/how-to-be-an-influencer.Rmd")
-rmarkdown::render_site()
